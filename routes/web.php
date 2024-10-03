33bf4bbe05beed5d09cc89207c7d47a1bc116fd9
joes <joeswaruwu@gmail.com>
2024-10-03 22:05:13 +0700

    create migrations n models

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {
-    return view('login');
+
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'index']);
+
 Route::get('/forgot-password', function(){
     return view('forgot-password');
 });
